be1c46b77fe2d57c988be27783ef16cf40b0bec8
FeCCC <railgun.long@foxmail.com>
2023-09-12 17:58:03 +0800

    projects add nvim pattern

--- .config/nvim/lua/plugin/plugins/project.lua
+++ .config/nvim/lua/plugin/plugins/project.lua
@@ -1,12 +1,12 @@
 local project_plugin = {
-	"ahmedkhalf/project.nvim",
-	config = function()
-		require("project_nvim").setup({
-			detection_methods = { "pattern" },
-			datapath = vim.fn.stdpath("data"),
-			patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".sln" },
-		})
-	end,
+    "ahmedkhalf/project.nvim",
+    config = function()
+        require("project_nvim").setup({
+            detection_methods = { "pattern" },
+            datapath = vim.fn.stdpath("data"),
+            patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "package.json", ".sln", "init.lua" },
+        })
+    end,
 }
 
 return project_plugin
